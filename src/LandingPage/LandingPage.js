bed7a8dfd7900c733d901217a2979193f56b6c54
tramdangthuy <tramdangthuy3011@gmail.com>
2021-05-11 20:37:20 +0700

    add search bar

--- src/LandingPage/LandingPage.js
+++ src/LandingPage/LandingPage.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import {TopNav} from './TopNav/TopNav';
+import {SearchBar} from './SearchBar/SearchBar';
+import logo from '../assets/logo.png';
+import styles from './LandingPage.module.css';
+
 export function LandingPage(){
     return(
         <div>
             <TopNav/>
+            <img src={logo} className={styles.logo} alt='logo'/>
+            <SearchBar/>
         </div>
     );
 }
